ea0f3b19ca44f05b3028989c9b5f55945c0dbe7a
Philippe Blondeel <philippe.blondeel@kuleuven.be>
2021-04-23 15:51:39 +0200

    update

--- Example/diffusion2D_Example_UnStructured_Triag.jl
+++ Example/diffusion2D_Example_UnStructured_Triag.jl
@@ -18,14 +18,14 @@ DiffusionCoefficient=10.0
 QuadPoints=19
 
 #Order 1
-Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_1_42.txt")))
+Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_1_198.txt")))
 Elements=Elements[:,5:end]
-Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_1_42.txt"))
+Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_1_198.txt"))
 Nodes1=Nodes[:,2:3]
 
-#figure()
-#gridPlot2D.plotElements(Elements,Nodes1,"Triag")
-#gridPlot2D.plotNodes(Nodes1)
+figure()
+gridPlot2D.plotElements(Elements,Nodes1,"Triag")
+gridPlot2D.plotNodes(Nodes1)
 
 ElemType="TwoD_Triag_Order1"
 NumberOfElements=size(Elements,1)
@@ -35,15 +35,15 @@ u1=solver2D.main(Nodes1,Elements,MaterialParam,solverparam)
 
 ####################################################################################3
 #Order 2
-Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_2_42.txt")))
+Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_2_198.txt")))
 Elements=Elements[:,5:end]
-Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_2_42.txt"))
+Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_2_198.txt"))
 Nodes2=Nodes[:,2:3]
 
 
-#figure()
-#gridPlot2D.plotElements(Elements,Nodes2,"Triag")
-#gridPlot2D.plotNodes(Nodes2)
+figure()
+gridPlot2D.plotElements(Elements,Nodes2,"Triag")
+gridPlot2D.plotNodes(Nodes2)
 
 ElemType="TwoD_Triag_Order2"
 NumberOfElements=size(Elements,1)
@@ -53,14 +53,14 @@ u2=solver2D.main(Nodes2,Elements,MaterialParam,solverparam)
 
 ####################################################################################3
 #Order 3
-Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_3_42.txt")))
+Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_3_198.txt")))
 Elements=Elements[:,5:end]
-Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_3_42.txt"))
+Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_3_198.txt"))
 Nodes3=Nodes[:,2:3]
 
-#figure()
-#gridPlot2D.plotElements(Elements,Nodes3,"Triag")
-#gridPlot2D.plotNodes(Nodes3)
+figure()
+gridPlot2D.plotElements(Elements,Nodes3,"Triag")
+gridPlot2D.plotNodes(Nodes3)
 
 ElemType="TwoD_Triag_Order3"
 NumberOfElements=size(Elements,1)
@@ -70,9 +70,9 @@ u3=solver2D.main(Nodes3,Elements,MaterialParam,solverparam)
 
 
 #Order 4
-#Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_4_42.txt")))
+#Elements=Int64.(readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Elements_4_198.txt")))
 #Elements=Elements[:,5:end]
-#Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_4_42.txt"))
+#Nodes=readdlm(joinpath(locationOfMesh,"2D/Unstructured/Triag/Nodes_4_198.txt"))
 #Nodes4=Nodes[:,2:3]
 
 #figure()
@@ -89,6 +89,19 @@ u3=solver2D.main(Nodes3,Elements,MaterialParam,solverparam)
 println(maximum(u1))
 println(maximum(u2))
 println(maximum(u3))
+println(@__DIR__())
+
+out1=open(string(@__DIR__(),"/out/2D/Unstructured/Triag/1.txt"),"w")
+ writedlm(out1,[Nodes1 u1])
+ close(out1)
+
+ out2=open(string(@__DIR__(),"/out/2D/Unstructured/Triag/2.txt"),"w")
+  writedlm(out2,[Nodes2 u2])
+  close(out2)
+
+  out3=open(string(@__DIR__(),"/out/2D/Unstructured/Triag/3.txt"),"w")
+   writedlm(out3,[Nodes3 u3])
+   close(out3)
 #println(maximum(u4))
 
 #println(size(Nodes1))
